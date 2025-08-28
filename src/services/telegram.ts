8b9c7fdcd06cefc020b7cf3f8ef1245e0636725a
Galih Setyawan <galih.kamvax032@gmail.com>
2025-08-28 11:31:31 +0700

    style 💎: update Telegram bot sending logic with Markdown support
    Added `parse_mode` option to the `sendMessage` method for better formatting and readability in Telegram messages.

--- src/services/telegram.ts
+++ src/services/telegram.ts
@@ -7,14 +7,18 @@ const bot = new TelegramBot(env.telegram.botToken);
 export const sendToTelegram = async (message: string) => {
   try {
 
-    await bot.sendMessage(env.telegram.mainChannelId, message);
+    await bot.sendMessage(env.telegram.mainChannelId, message, {
+      parse_mode: "Markdown",
+    });
     if (env.telegram.threadId) {
       await bot.sendMessage(env.telegram.channelId, message, {
         message_thread_id: parseInt(env.telegram.threadId),
         parse_mode: "Markdown",
       });
     } else {
-      await bot.sendMessage(env.telegram.channelId, message);
+      await bot.sendMessage(env.telegram.channelId, message, {
+        parse_mode: "Markdown",
+      });
     }
   } catch (err) {
     logger.error("Failed to send to Telegram:", err);
